b8e728054915859298df4c6abf6d419638cb4306
saintyinyin <gangzhiyin@hotmail.com>
2021-08-13 21:47:58 +0800

    templates ex.

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(response):
-    return HttpResponse('ok')
+def index(request):
+    #return HttpResponse('ok')
+    context = {
+        'name' : 'Show me the money!'
+    }
+    return render(request, "book/index.html", context = context)
