a7a0ae5e1d2b1e09be96932b691db88e577750ee
saintyinyin <gangzhiyin@hotmail.com>
2021-08-13 21:03:14 +0800

    add url change

--- bookmanager/bookmanager/urls.py
+++ bookmanager/bookmanager/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('index/', index),
-    path('book/', include('book.urls')),
+    #path('book/', include('book.urls')),
+    path('', include('book.urls')),
 ]
